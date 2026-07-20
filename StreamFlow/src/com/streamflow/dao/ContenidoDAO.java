96d0a1e3ac2ce658f29bd0b02fc4b45d176e620d
PabloLeon2005 <pabloguaman2005@gmail.com>
2026-07-19 19:29:18 -0500

    DAO Contenido

--- StreamFlow/src/com/streamflow/dao/ContenidoDAO.java
+++ StreamFlow/src/com/streamflow/dao/ContenidoDAO.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.streamflow.dao;
 
-/**
- *
- * @author PC
- */
-public interface ContenidoDAO {
-    
+import com.streamflow.model.Contenido;
+
+public interface ContenidoDAO extends Crud<Contenido> {
 }
